fa5bc0083aa57d434de77b52d9dcb85b166650e3
wuhuyoung <1138841120@qq.com>
2022-11-10 16:50:35 +0800

    使用spring cache来缓存套餐数据

--- reggie_take_out/src/main/java/com/example/reggie/common/Result.java
+++ reggie_take_out/src/main/java/com/example/reggie/common/Result.java
@@ -1,11 +1,12 @@
 package com.example.reggie.common;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
-public class Result<T>{
+public class Result<T> implements Serializable {
     private Integer code; //编码：1成功，0和其它数字为失败
     private String msg; //错误信息
     private T data; //数据
